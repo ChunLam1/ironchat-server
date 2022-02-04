4d0cc311df330f05c8492f15f049068706b86290
Chun Lam <Chun01@hotmail.fr>
2022-02-04 17:28:07 +0100

    ProfileRouteFixed

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const UserModel = require("../models/User.model");
 const protectRoute = require("../middlewares/protectRoute");
+const jwt = require("jsonwebtoken")
 
 router.get("/", (req, res) => {
   UserModel.find()
@@ -10,11 +11,16 @@ router.get("/", (req, res) => {
     .catch((e) => console.error(e));
 });
 
-router.patch("/", (req, res, next) => {
-  UserModel.findByIdAndUpdate(req.body._id, req.body, { new: true })
-    .then((user) => {
-      console.log(req);
-      res.status(200).json(user);
+router.patch("/:id", (req, res, next) => {
+  UserModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((newUser) => {
+      const user = newUser.toObject();
+      delete user.password;
+      const newAuthToken = jwt.sign(user, process.env.TOKEN_SECRET, {
+        algorithm: "HS256",
+        expiresIn: "5d",
+      });
+      res.status(200).json({ newAuthToken });
     })
     .catch((e) => console.log(e));
 });
