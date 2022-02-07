3f45b5d1256865ec5a0b4c8314f9981863cf3c82
Chun Lam <Chun01@hotmail.fr>
2022-02-07 12:14:18 +0100

    new message

--- socket/index.js
+++ socket/index.js
@@ -3,14 +3,27 @@ module.exports = function (app) {
   const io = require("socket.io")(httpServer, {
     cors: { origin: process.env.FRONTEND_URL },
   });
+  const Message = require("../models/Message.model")
 
   console.log(io);
 
   io.on("connection", (socket) => {
-    socket.on("message", (arg) => {
-      console.log(arg);
+    console.log("a user is connected");
+
+    socket.on("disconnected", () => {
+      console.log("a user is disconnected");
     });
-    socket.emit("toto", { data: ":)" });
+
+    socket.on("message", (msg) => {
+      console.log("message reçu : " + msg.message);
+
+      Message.create()
+    });
+
+    // socket.on("message", (arg) => {
+    //   console.log(arg);
+    // });
+    socket.emit("toto", () => console.log("hello"));
   });
 
   httpServer.listen(process.env.SOCKET_PORT);
