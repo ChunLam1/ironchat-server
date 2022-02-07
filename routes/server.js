80125ea9da68d42169d63f1f95ceebbb11641272
Tom <eizandev@gmail.com>
2022-02-07 15:04:43 +0100

    working on servers

--- routes/server.js
+++ routes/server.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const ServerModel = require("../models/Server.model");
 const protectRoute = require("../middlewares/protectRoute");
 const MessageModel = require("../models/Message.model");
+const User = require("../models/User.model");
 
 router.get("/", (req, res) => {
   ServerModel.find()
@@ -12,13 +13,22 @@ router.get("/", (req, res) => {
 });
 
 // :id correspond au serverId
-router.get("/:id/messages", (req, res) => {
-  MessageModel
-    .find()
-    .populate("serverId", "userId")
-    .then((message) => res.status(200).json({ message }))
-    .catch(e => console.error(e))
-})
+router.get("/:id/messages", async (req, res, next) => {
+  try {
+    const serverId = req.params.id;
+    console.log(serverId);
+    const messages = await MessageModel.find({
+      serverId: req.params.id,
+    })
+      .populate("userId")
+      .populate("serverId");
+    // console.log(messages);
+    res.status(200).json({ messages });
+    // if (req.params.id === )
+  } catch (e) {
+    console.error(e);
+  }
+});
 
 router.post("/", (req, res, next) => {
   ServerModel.create(req.body)
