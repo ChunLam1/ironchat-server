d8e7b22f75950df521081920368bf830914060e6
Tom <eizandev@gmail.com>
2022-02-10 12:40:15 +0100

    heroku fix

--- socket/index.js
+++ socket/index.js
@@ -5,7 +5,10 @@ module.exports = function (app) {
   });
   const Message = require("../models/Message.model");
 
-  console.log(io);
+  io.configure(function () {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+  });
 
   io.on("connection", (socket) => {
     console.log("a user is connected");
